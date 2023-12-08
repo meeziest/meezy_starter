e92e388cd8fb1f47172cad387f91016c1a419bb4
Dinmukhammed <dinmukhammed.yerimbetov@gmail.com>
2023-12-08 23:41:48 +0600

    added default assret bundle

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'features/app/presentation/app_screen.dart';
 
 void main() {
-  runApp(const MeezyApp());
+  final bindings = WidgetsFlutterBinding.ensureInitialized()..deferFirstFrame();
+
+  FlutterNativeSplash.preserve(widgetsBinding: bindings);
+
+  /// await some initialization stuff
+  bindings.allowFirstFrame();
+  FlutterNativeSplash.remove(); // end splash screen
+
+  runApp(
+    DefaultAssetBundle(
+      bundle: SentryAssetBundle(),
+      child: const MeezyApp(),
+    ),
+  );
 }
